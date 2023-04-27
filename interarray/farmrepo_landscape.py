c3e0812bae1ce50cc9478fd1e5da285f6c6fa622
mdealencar <DTUnote+mdealencar@users.noreply.github.com>
2023-04-27 10:08:40 +0200

    added landscape farmrepo

--- interarray/farmrepo_landscape.py
+++ interarray/farmrepo_landscape.py
@@ -33,30 +33,29 @@ def tess3sm(radius, spacing=1000):
     RootC = 2.5*spacing*np.array(((-0.5, -h), (-0.5, h), (1., 0.)))
     return synthfarm2graph(RootC, NodeC, name='SynthTess small (3 OSS)')
 
-
 datapath = Path(__file__).resolve().parent.parent / 'data'
 
 g = namedtuplify(
     namedtuple_typename='FarmGraphs',
     # .xlsx files
     # 100 WTG
-    thanet=file2graph(datapath / 'Thanet.xlsx'),
+    thanet=file2graph(datapath / 'Thanet.xlsx', rotate=49),
     # 80 WTG
-    dantysk=file2graph(datapath / 'DanTysk.xlsx'),
+    dantysk=file2graph(datapath / 'DanTysk.xlsx', rotate=90),
     # 80 WTG
     horns=file2graph(datapath / 'Horns Rev 1.xlsx'),
     # 111 WTG
-    anholt=file2graph(datapath / 'Anholt.xlsx'),
+    anholt=file2graph(datapath / 'Anholt.xlsx', rotate=84),
     # 108 WTG
-    sands=file2graph(datapath / 'West of Duddon Sands.xlsx'),
+    sands=file2graph(datapath / 'West of Duddon Sands.xlsx', rotate=55),
     # 30 WTG
-    ormonde=file2graph(datapath / 'Ormonde.xlsx'),
+    ormonde=file2graph(datapath / 'Ormonde.xlsx', rotate=45),
     # 175 WTG, 2 OSS
-    london=file2graph(datapath / 'London Array.xlsx'),
+    london=file2graph(datapath / 'London Array.xlsx', rotate=-95),
     # 27 WTG
-    rbn=file2graph(datapath / 'BIG Ronne Bank North.xlsx'),
+    rbn=file2graph(datapath / 'BIG Ronne Bank North.xlsx', rotate=-4),
     # 53 WTG
-    rbs=file2graph(datapath / 'BIG Ronne Bank South.xlsx'),
+    rbs=file2graph(datapath / 'BIG Ronne Bank South.xlsx', rotate=-2),
 
     # synthetic farms
     # 114 WTG
