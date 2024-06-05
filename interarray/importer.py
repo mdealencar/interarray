575f6b715407b4e5428808900a2b18b681c39316
Mauricio Souza de Alencar <856825+mdealencar@users.noreply.github.com>
2024-06-05 22:23:55 +0200

    feat: add doggerA wind farm to repository (Dogger Bank A.yaml, importer.py)

--- interarray/importer.py
+++ interarray/importer.py
@@ -81,6 +81,7 @@ _site_handles = dict(
     borkum2='Borkum Riffgrund 2',
     borssele='Borssele',
     dantysk='DanTysk',
+    doggerA='Dogger Bank A',
     dudgeon='Dudgeon',
     anglia='East Anglia ONE',
     gode='Gode Wind 1',
