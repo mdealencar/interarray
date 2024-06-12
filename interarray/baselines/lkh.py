ab52d7f7d28e3c3307601410ad3344cd069f72ba
mdealencar <856825+mdealencar@users.noreply.github.com>
2024-06-12 18:20:45 +0200

    feat: LKH interface improvements (__init__.py, lkh.py)

--- interarray/baselines/lkh.py
+++ interarray/baselines/lkh.py
@@ -1,9 +1,12 @@
 import math
 import time
 import os
+import re
 import io
 import tempfile
 import subprocess
+from pathlib import Path
+from typing import TextIO
 import networkx as nx
 import numpy as np
 
@@ -13,29 +16,50 @@ from ..pathfinding import PathFinder
 from ..geometric import make_graph_metrics, delaunay
 
 
-def write_edge_file(G: nx.Graph, f) -> None:
+# TODO: Deprecate that. Unable to make LKH work in ACVRP with EDGE_FILE
+#       It seems like EDGE_FILE is for candidate edges of the transformed
+#       (symmetric) problem (i.e., LKH expects a higher node count)
+def make_edge_listing(A: nx.Graph, precision_factor: float) -> str:
     '''
-    LKH's EDGE_FILE param
+    LKH's EDGE_FILE param (follows Concorde format)
     node numbering starts from 0
-    every node number is incremented by 1 when loaded
+    every node number will be incremented by 1 when loaded by LKH
     '''
-    pass
+    V = A.number_of_nodes()
+    N = V - A.graph['M']
+    A_nodes = nx.subgraph_view(A, filter_node=lambda n: n >= 0)
+    return '\n'.join((
+        # first line is <number_of_nodes> <number_of_edges>
+        f'{V} {2*A_nodes.number_of_edges() + 2*N}',
+        # edges not including the depot (symmetric)
+        '\n'.join('{s} {t} {cost:.0f}\n{t} {s} {cost:.0f}'.format(
+            s=u+1, t=v+1, cost=precision_factor*d)
+                  for u, v, d in A_nodes.edges(data='length')),
+        # depot connects to all nodes, but asymmetrically (zero-cost return)
+        '\n'.join(f'{1} {n} {precision_factor*d:.0f}\n{n} 1 0'
+                  for n, d in enumerate(A.graph['d2roots'][:, 0], start=1)),
+        ))
 
 
-def lkh_acvrp(G_base: nx.Graph, *, capacity: int, time_limit: int, runs: int = 50,
-              precision_factor: float = 1e8, complete=False, per_run_limit: float = 5
-              ) -> nx.Graph:
+def lkh_acvrp(G_base: nx.Graph, *, capacity: int, time_limit: int,
+              runs: int = 50, precision_factor: float = 1e4, complete=False,
+              per_run_limit: float = 15.) -> nx.Graph:
     G = nx.create_empty_copy(G_base)
     M = G.graph['M']
-    assert M == 1  # LKH allows only 1 depot
+    assert M == 1, 'LKH allows only 1 depot'
+    problem_fname = 'problem.txt'
+    params_fname = 'params.txt'
+    edge_fname = 'edge_file.txt'
     VertexC = G.graph['VertexC']
     N = VertexC.shape[0] - M
     weights, A = weight_matrix_single_depot_from_G(
             G, precision_factor=precision_factor, complete=complete)
+    #  weights = np.ones((N + M, N + M), dtype=int)
     with io.StringIO() as str_io:
         np.savetxt(str_io, weights, fmt='%d')
         edge_weights = str_io.getvalue()[:-1]
 
+    #  print(edge_weights)
     output_fname = 'solution.out'
     specs = dict(
         NAME=G.graph.get('name', 'unnamed'),
@@ -52,6 +76,17 @@ def lkh_acvrp(G_base: nx.Graph, *, capacity: int, time_limit: int, runs: int = 5
         DEPOT_SECTION=f'1\n-1',
         EDGE_WEIGHT_SECTION=edge_weights,
     )
+    if False:
+        specs['EDGE_DATA_FORMAT'] = 'ADJ_LIST'
+        A_nodes = nx.subgraph_view(A, filter_node=lambda n: n >= 0)
+        data['EDGE_DATA_SECTION'] = '\n'.join((
+            # depot has out-edges to all nodes
+            f'1 {" ".join(str(n) for n in range(2, N + 2))} -1',
+            # all node have out-edges to depot
+            '\n'.join(f'{n + 2} 1 {" ".join(str(a + 2) for a in adj)} -1'
+                      for n, adj in A_nodes.adjacency()),
+            '-1'
+            ))
     #  if not complete:
     if False:  # adding edges just makes the solver fail
         specs['EDGE_DATA_FORMAT'] = 'EDGE_LIST'
@@ -65,6 +100,7 @@ def lkh_acvrp(G_base: nx.Graph, *, capacity: int, time_limit: int, runs: int = 5
     data_str = '\n'.join(f'{k}\n{v}' for k, v in data.items()) + '\nEOF'
     params = dict(
         SPECIAL=None,  # None -> output only the key
+        PRECISION=1000,  # d[i][j] = PRECISION*c[i][j] + pi[i] + pi[j]
         TOTAL_TIME_LIMIT=time_limit,
         TIME_LIMIT=per_run_limit,
         RUNS=runs,  # default: 10
@@ -82,32 +118,47 @@ def lkh_acvrp(G_base: nx.Graph, *, capacity: int, time_limit: int, runs: int = 5
         #  KICK_TYPE=4,
         #  MAX_SWAPS=0,
         #  POPULATION_SIZE=12,  # default 10
-        #  EDGE_FILE=  # Concorde format, maybe can replace EDGE_WEIGHT_SECTION and EDGE_DATA_SECTION
+        #  PATCHING_A=
+        #  PATCHING_C=
     )
-
-    problem_fname = 'problem.txt'
-    params_fname = 'params.txt'
+    #  edge_str = make_edge_listing(delaunay(G), precision_factor)
     with tempfile.TemporaryDirectory() as tmpdir:
         problem_fpath = os.path.join(tmpdir, problem_fname)
+        #  edge_fpath = os.path.join(tmpdir, edge_fname)
         params_fpath = os.path.join(tmpdir, params_fname)
         params['PROBLEM_FILE'] = problem_fpath
+        #  params['EDGE_FILE'] = edge_fpath
         params['MTSP_SOLUTION_FILE'] = os.path.join(tmpdir,
                                                     output_fname)
-        with open(problem_fpath, 'w') as f_problem:
-            f_problem.write('\n'.join((spec_str, data_str)))
-        with open(params_fpath, 'w') as f_params:
-            f_params.write('\n'.join((f'{k} = {v}' if v is not None else k)
-                                     for k, v in params.items()))
+        Path(problem_fpath).write_text('\n'.join((spec_str, data_str)))
+        Path(params_fpath).write_text('\n'.join(
+            (f'{k} = {v}' if v is not None else k) for k, v in params.items()))
+        #  Path(edge_fpath).write_text(edge_str)
         start_time = time.perf_counter()
         result = subprocess.run(['LKH', params_fpath], capture_output=True)
         run_time = time.perf_counter() - start_time
-        print('===stdout===', result.stdout.decode('utf8'), sep='\n')
-        print('===stderr===', result.stderr.decode('utf8'), sep='\n')
         with open(os.path.join(tmpdir, output_fname), 'r') as f_sol:
-            metric, optimum = next(f_sol).split(':')[-1][:-1].split('_')
+            penalty, minimum = next(f_sol).split(':')[-1][:-1].split('_')
             next(f_sol)  # discard second line
             branches = [[int(node) - 2 for node in line.split(' ')[1:-5]]
                         for line in f_sol]
+    #  print('===stdout===', result.stdout.decode('utf8'), sep='\n')
+    #  print('===stderr===', result.stderr.decode('utf8'), sep='\n')
+    tail = result.stdout[result.stdout.rfind(b'Cost.min'):].decode('ascii')
+    entries = iter(tail.splitlines())
+    cost = tuple(float(v) for v in rre.match(
+        r'Cost\.min = (\d+), Cost\.avg = (\d+\.?\d*), Cost\.max = (\d+)',
+        next(entries)).groups())
+    next(entries)  # skip gap line
+    penalty = tuple(float(v) for v in rre.match(
+        r'Penalty\.min = (\d+), Penalty\.avg = (\d+\.?\d*), Penalty\.max = (\d+)',
+        next(entries)).groups())
+    trials = tuple(float(v) for v in rre.match(
+        r'Trials\.min = (\d+), Trials\.avg = (\d+\.?\d*), Trials\.max = (\d+)',
+        next(entries)).groups())
+    time = tuple(float(v) for v in re.match(
+        r'Time\.min = (\d+\.?\d*), Time\.avg = (\d+\.?\d*), Time\.max = (\d+\.?\d*)',
+        next(entries)).groups())
     d2roots = G.graph['d2roots']
     #  return G, branches
     VertexC = G.graph['VertexC']
@@ -129,11 +180,12 @@ def lkh_acvrp(G_base: nx.Graph, *, capacity: int, time_limit: int, runs: int = 5
                 nonAedges.append((s, t))
             s = t
     calcload(G)
-    PathFinder(G).create_detours(in_place=True)
+    if not nonAedges:
+        PathFinder(G).create_detours(in_place=True)
     make_graph_metrics(G)
     G.graph['nonAedges'] = nonAedges
-    G.graph['metric'] = int(metric)
-    G.graph['optimum'] = int(optimum)
+    G.graph['penalty'] = int(penalty)
+    G.graph['minimum'] = int(minimum)
     # G.graph['iterations'] = ???
     G.graph['capacity'] = capacity
     # G.graph['overfed'] = [len(G[root])/np.ceil(N/capacity)*M
