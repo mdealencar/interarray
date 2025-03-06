fb8c62981bfde62de82b229c249317da57f231ca
Mauricio Souza de Alencar <856825+mdealencar@users.noreply.github.com>
2025-03-06 15:37:30 +0100

    feat: implement infobox in svgplot() plus several tweaks (svgplot.py)

--- interarray/svg.py
+++ interarray/svg.py
@@ -11,7 +11,7 @@ from ground.base import get_context
 import svg
 
 from .geometric import rotate
-from .interarraylib import calcload, describe_G
+from .interarraylib import describe_G
 
 
 class SvgRepr():
@@ -31,7 +31,8 @@ class SvgRepr():
             file.write(self.data)
 
 
-def svgplot(G, landscape=True, dark=None, node_size=12):
+def svgplot(G, landscape=True, dark=None, infobox: bool = True,
+            node_size: int = 12, github_bugfix: bool = True):
     '''Make a NetworkX graph representation directly in SVG.
 
     Because matplotlib's svg backend does not make efficient use of SVG
@@ -70,12 +71,13 @@ def svgplot(G, landscape=True, dark=None, node_size=12):
     H = max(VertexC[:idx_B, 1].max(), VertexC[-R:, 1].max()) - Hoff
     wr = (w - 2*margin)/W
     hr = (h - 2*margin)/H
-    if wr/hr < w/h:
+    if W/H < w/h:
+        # tall aspect
+        r = hr
+    else:
+        # wide aspect
         r = wr
         h = round(H*r + 2*margin)
-    else:
-        r = hr
-        #  w = round(W*r + 2*margin)
     offset = np.array((Woff, Hoff))
     VertexS = (VertexC - offset)*r + margin
     # y axis flipping
@@ -86,34 +88,29 @@ def svgplot(G, landscape=True, dark=None, node_size=12):
     kind2alpha = defaultdict(lambda: 1.)
     kind2alpha['virtual'] = 0.4
     kind2color = {}
-    kind2style = dict(
-        detour='dashed',
-        scaffold='dotted',
-        delaunay='solid',
-        extended='dashed',
-        tentative='dashed',
-        rogue='dashed',
-        contour='solid',
-        contour_delaunay='solid',
-        contour_extended='dashed',
-        border='dashed',
-        virtual='solid',
-        unspecified='solid',
+    kind2dasharray = dict(
+            tentative='18 15',
+            rogue='25 5',
+            extended='18 15',
+            contour_extended='18 15',
+            scaffold='10 10',
     )
     if dark:
         kind2color.update(
-            detour='darkorange',
             scaffold='gray',
             delaunay='darkcyan',
             extended='darkcyan',
             tentative='red',
             rogue='yellow',
-            contour='red',
             contour_delaunay='green',
             contour_extended='green',
+            contour='red',
+            planar='darkorchid',
+            constraint='purple',
             border = 'silver',
-            virtual='gold',
             unspecified='crimson',
+            detour='darkorange',
+            virtual='gold',
         )
         text_color = 'white'
         root_color = 'lawngreen'
@@ -122,18 +119,20 @@ def svgplot(G, landscape=True, dark=None, node_size=12):
         border_face = '#111'
     else:
         kind2color.update(
-            detour='royalblue',
             scaffold='gray',
-            delaunay='black',
-            extended='black',
-            tentative='magenta',
-            rogue='darkorange',
-            contour='magenta',
-            contour_delaunay='darkgreen',
-            contour_extended='darkgreen',
+            delaunay='darkgreen',
+            extended='darkgreen',
+            tentative='darkorange',
+            rogue='magenta',
+            contour_delaunay='firebrick',
+            contour_extended='firebrick',
+            contour='black',
+            planar='darkorchid',
+            constraint='darkcyan',
             border = 'dimgray',
-            virtual='gold',
             unspecified='firebrick',
+            detour='royalblue',
+            virtual='gold',
         )
         text_color = 'black'
         root_color = 'black'
@@ -151,53 +150,36 @@ def svgplot(G, landscape=True, dark=None, node_size=12):
         fnT = np.arange(R + T + B + 3)
         fnT[-R:] = range(-R, 0)
 
+    #############################
+    # generate the SVG elements #
+    #############################
+    styleEntries = []
+    # elements should be added according to the desired z-order
+    graphElements = []
+
+    # prepare obstacles
     draw_obstacles = []
     if obstacles is not None:
         for obstacle in obstacles:
             draw_obstacles.append(
                 'M' + ' '.join(str(c) for c in VertexS[obstacle].flat) + 'z')
-    borderE = svg.Path(
-        id='border',
-        stroke=kind2color['border'],
-        stroke_dasharray=(15, 7),
-        fill=border_face,
-        # fill_rule "evenodd" is agnostic to polygon vertices orientation
-        # "nonzero" would depend on orientation (if opposite, no fill)
-        fill_rule="evenodd",
-        d=' '.join(chain(
-            ('M' + ' '.join(str(c) for c in VertexS[border].flat) + 'z',),
-            draw_obstacles
-        )),
-    )
-
-    if (not G.graph.get('has_loads', False)
-            and G.number_of_edges() == T + C + D):
-        calcload(G)
-
-    # wtg nodes
-    subtrees = defaultdict(list)
-    for n, sub in G.nodes(data='subtree', default=19):
-        if 0 <= n < T:
-            subtrees[sub].append(n)
-    svgnodes = []
-    for sub, nodes in subtrees.items():
-        svgnodes.append(svg.G(
-            fill=colors[sub % len(colors)],
-            elements=[svg.Use(href='#wtg', x=VertexS[n, 0], y=VertexS[n, 1])
-                      for n in nodes]))
-    svgnodesE = svg.G(id='WTGgrp', elements=svgnodes)
-
-    # oss nodes
-    svgrootsE = svg.G(
-        id='OSSgrp',
-        elements=[svg.Use(href='#oss', x=VertexS[r, 0] - root_side/2,
-                          y=VertexS[r, 1] - root_side/2)
-                  for r in range(-R, 0)])
-    # Detour nodes
-    svgdetoursE = svg.G(
-        id='DTgrp', elements=[svg.Use(href='#dt', x=VertexS[d, 0],
-                                      y=VertexS[d, 1])
-                              for d in fnT[T + B + C: T + B + C + D]])
+    # border with obstacles as holes
+    if border is not None:
+        borderE = svg.Path(
+            id='border',
+            stroke=kind2color['border'],
+            stroke_dasharray=[15, 7],
+            stroke_width=2,
+            fill=border_face,
+            # fill_rule "evenodd" is agnostic to polygon vertices orientation
+            # "nonzero" would depend on orientation (if opposite, no fill)
+            fill_rule='evenodd',
+            d=' '.join(chain(
+                ('M' + ' '.join(str(c) for c in VertexS[border].flat) + 'z',),
+                draw_obstacles
+            )),
+        )
+        graphElements.append(borderE)
 
     # Edges
     class_dict = {'delaunay': 'del',
@@ -208,20 +190,39 @@ def svgplot(G, landscape=True, dark=None, node_size=12):
                   'contour_extended': 'coe',
                   'extended': 'ext',
                   'scaffold': 'scf',
-                  None: 'std'}
+                  'unspecified': 'std'}
     edges_with_kind = G.edges(data='kind')
     edge_lines = defaultdict(list)
+    edge_kinds_used = set()
     for u, v, edge_kind in edges_with_kind:
         if edge_kind == 'detour':
+            # detours are drawn separately as polylines
             continue
+        if edge_kind is None:
+            edge_kind = 'unspecified'
+        edge_kinds_used.add(edge_kind)
+        u, v = (u, v) if u < v else (v, u)
         edge_lines[class_dict[edge_kind]].append(
             svg.Line(x1=VertexS[fnT[u], 0], y1=VertexS[fnT[u], 1],
                      x2=VertexS[fnT[v], 0], y2=VertexS[fnT[v], 1]))
-    edgesE_ = [svg.G(id='edges', class_=class_, elements=lines)
-               for class_, lines in edge_lines.items()]
-    # Detour edges as polylines (to align the dashes among overlapping lines)
-    Points = []
-    if D:
+    if edge_lines:
+        styleEntries.append(f'line {{stroke-width: 4}}')
+        for edge_kind in edge_kinds_used:
+            attrs = dict(stroke=kind2color[edge_kind])
+            if edge_kind in kind2dasharray:
+                attrs['stroke-dasharray'] = kind2dasharray[edge_kind]
+            styleEntries.append(
+                f'.{class_dict[edge_kind]} '
+                f'{{{"; ".join(f"{k}: {v}" for k, v in attrs.items())}}}'
+            )
+        edgesE_ = [svg.G(id='edges', class_=[class_], elements=lines)
+                   for class_, lines in edge_lines.items()]
+        graphElements.extend(edgesE_)
+
+    # detour elements
+    if D > 0:
+        # Detour edges as polylines (to align the dashes among overlapping lines)
+        Points = []
         for r in range(-R, 0):
             detoured = [n for n in G.neighbors(r) if n >= T + B + C]
             for t in detoured:
@@ -236,54 +237,87 @@ def svgplot(G, landscape=True, dark=None, node_size=12):
                         break
                     s, t = t, u
                 Points.append(' '.join(str(c) for c in VertexS[hops].flat))
-    if Points:
         edgesdtE = svg.G(
-            id='detours', class_='dt',
+            id='detours', class_=['dt'],
             elements=[svg.Polyline(points=points) for points in Points])
-    else:
-        edgesdtE = []
+        graphElements.append(edgesdtE)
+
+        # Detour nodes
+        svgdetoursE = svg.G(
+            id='DTgrp', elements=[
+                svg.Use(href='#dt', x=VertexS[d, 0], y=VertexS[d, 1])
+                for d in fnT[T + B + C: T + B + C + D]]
+        )
+        graphElements.append(svgdetoursE)
+        styleEntries.extend((
+            f'polyline {{stroke-width: 4}}',
+            f'.dt {{stroke-dasharray: 18 15; fill: none; '
+                f'stroke: {kind2color["detour"]}}}',
+        ))
+
+    # wtg nodes
+    subtrees = defaultdict(list)
+    for n, sub in G.nodes(data='subtree', default=19):
+        if 0 <= n < T:
+            subtrees[sub].append(n)
+    svgnodes = []
+    for sub, nodes in subtrees.items():
+        svgnodes.append(svg.G(
+            fill=colors[sub % len(colors)],
+            elements=[svg.Use(href='#wtg', x=VertexS[n, 0], y=VertexS[n, 1])
+                      for n in nodes]))
+    svgnodesE = svg.G(id='WTGgrp', elements=svgnodes)
+    graphElements.append(svgnodesE)
+
+    # oss nodes
+    svgrootsE = svg.G(
+        id='OSSgrp',
+        elements=[svg.Use(href='#oss', x=VertexS[r, 0] - root_side/2,
+                          y=VertexS[r, 1] - root_side/2)
+                  for r in range(-R, 0)])
+    graphElements.append(svgrootsE)
 
     # Defs (i.e. reusable elements)
-    reusableE = svg.Defs(elements=[
+    reusableE = [
         svg.Circle(id='wtg', stroke=node_edge, stroke_width=2, r=node_size),
         svg.Rect(id='oss', fill=root_color, stroke=node_edge, stroke_width=2,
                  width=root_side, height=root_side),
-        svg.Circle(id='dt', fill='none', stroke_opacity=0.3,
-                   stroke=detour_ring, stroke_width=4, r=23),
-    ])
+    ]
+    if D > 0:
+        reusableE.append(svg.Circle(id='dt', fill='none', stroke_opacity=0.3,
+                         stroke=detour_ring, stroke_width=4, r=23))
 
-    # Info text
-    linesE = [svg.TSpan(x=w, dx="-0.2em", dy=f"-{0.2 + (i > 0)}em", text=line)
-             for i, line in enumerate(describe_G(G)[::-1])]
-    textE = svg.Text(x=w, y=h, elements=linesE, fill=text_color, font_size=40,
+    # Aggregate the SVG root elements
+    rootElements = [
+        svg.Style(text=' '.join(styleEntries)), svg.Defs(elements=reusableE),
+        svg.G(id=G.graph.get('handle', G.graph.get('name', 'handleless')),
+              elements=graphElements),
+        ]
+
+    # Infobox
+    if infobox and G.graph.get('has_loads', False):
+        w_drawn = round(W*r + 2*margin)
+        desc_lines = describe_G(G)[::-1]
+
+        if github_bugfix:
+            # this is a workaround for GitHub's bug in rendering svg utf8 text
+            # (only when the svg is inside an ipynb notebook)
+            desc_lines = [l.encode('ascii', 'xmlcharrefreplace').decode()
+                          for l in desc_lines]
+
+        linesE = [
+            svg.TSpan(x=w_drawn, dx=svg.Length(-0.2, 'em'),
+                      dy=svg.Length((-1.2 if i else -0.2), 'em'), text=line)
+            for i, line in enumerate(desc_lines)
+        ]
+        rootElements.append(
+            svg.Text(x=w_drawn, y=h, elements=linesE, fill=text_color, font_size=40,
                      text_anchor='end', font_family='sans-serif')
-
-    # Style
-    # TODO: use kind2style below
-    styleE = svg.Style(text=(
-        f'polyline {{stroke-width: 4}} '
-        f'line {{stroke-width: 4}} '
-        f'.std {{stroke: {kind2color["unspecified"]}}} '
-        f'.del {{stroke: {kind2color["delaunay"]}}} '
-        f'.con {{stroke: {kind2color["contour"]}}} '
-        f'.cod {{stroke: {kind2color["contour_delaunay"]}}} '
-        f'.coe {{stroke: {kind2color["contour_extended"]}; '
-        f'stroke-dasharray: 18 15}} '
-        f'.ttt {{stroke: {kind2color["tentative"]}; stroke-dasharray: 18 15}} '
-        f'.rog {{stroke: {kind2color["rogue"]}; stroke-dasharray: 25 5}} '
-        f'.ext {{stroke: {kind2color["extended"]}; stroke-dasharray: 18 15}} '
-        f'.scf {{stroke: {kind2color["scaffold"]}; stroke-dasharray: 10 10}} '
-        f'.dt {{stroke-dasharray: 18 15; fill: none; '
-        f'stroke: {kind2color["detour"]}}}'))
+        )
 
     # Aggregate all elements in the SVG figure.
     out = svg.SVG(
         viewBox=svg.ViewBoxSpec(0, 0, w, h),
-        elements=[
-            styleE, reusableE,
-            svg.G(id=G.graph.get('handle', G.graph.get('name', 'handleless')),
-                  elements=[borderE, *edgesE_, edgesdtE, svgnodesE,
-                            svgrootsE, svgdetoursE, textE])
-        ]
+        elements=rootElements,
     )
     return SvgRepr(out.as_str())
